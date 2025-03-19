6919d9c7a3185658407b50a1e258e1b2566b017a
Dinesh Muppidi <muppididinesh@outlook.com>
2025-03-19 15:37:41 -0500

    Added Heroku config files and port fix

--- password_checker.py
+++ password_checker.py
@@ -1,3 +1,4 @@
+import os
 import random
 import string
 import numpy as np
@@ -89,4 +90,5 @@ def home():
     return render_template("index.html", result=result, generated_password=generated_password)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
